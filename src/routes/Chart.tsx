ccce7c3b015b00ec11c7d834a001f5fea702f2a9
mocakosan@naver.com <mocakosan@naver.com>
2022-09-06 08:47:19 +0900

    first commit

---
+++ src/routes/Chart.tsx
@@ -0,0 +1,13 @@
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+
+interface ChartProps {
+  coinId: string;
+}
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery(["ohlcv",coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+  return <h1>chart</h1>
+}
+export default Chart;
