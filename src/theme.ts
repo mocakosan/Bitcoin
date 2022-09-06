46d44cd0297d14b768e35934778cbfce43dbd0b1
mocakosan@naver.com <mocakosan@naver.com>
2022-09-06 17:17:17 +0900

    feat: skin color

--- src/theme.ts
+++ src/theme.ts
@@ -1,14 +1,14 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "2f3640",
+  bgColor: "black",
   textColor: "white",
   accentColor: "#9c88ff",
   cardBgColor: "transparent",
 };
 
 export const lightTheme: DefaultTheme = {
-  bgColor: "whitesmoke",
+  bgColor: "#dbdbdb",
   textColor: "black",
   accentColor: "#9c88ff",
   cardBgColor: "white",
